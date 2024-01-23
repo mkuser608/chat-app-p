5afd0b487565164b577ac6d5051fd3570866a986
Pragyan Prava Swin <pragyanpravaswain@gmail.com>
2024-01-23 16:02:25 +0530

    mysql connection and create entity

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express(); // this is the instance of express, inside that we have get, post, delete, put, patch
 
-const uploadRout = require("../routs/router");
+//import bodyPraser
+const bodyParser = require('body-parser'); // we need to use it as a middleWare
+
+const uploadRout = require("./routs/upload");
+
+app.use(bodyParser.json());
 
 // we use use() as a middleware for incoming request
-app.use("/router", uploadRout);
+app.use("/upload", uploadRout);
 
 //inside the get method we have to pass the 2 parameter that is rout(/) & call back function and 
 //call back function has two parameter (request and response). 
